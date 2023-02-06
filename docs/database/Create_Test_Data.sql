2a1d71fd7d45831d618ef0112b71649899ed150d
Korbin Dansie <dansiekm@gmail.com>
2023-02-06 12:16:11 -0700

    change " to '

--- docs/database/Create_Test_Data.sql
+++ docs/database/Create_Test_Data.sql
@@ -62,28 +62,28 @@ SELECT * FROM `user`;
 ***************************************************************/
 SELECT '******* Deposit 200 *******' as '';
 SET @TransferSuccess = 0;
-CALL `deposit`(@accountNumber1, 1, 200, "A deposit", @TransferSuccess);
+CALL `deposit`(@accountNumber1, 1, 200, 'A deposit', @TransferSuccess);
 
 SELECT '******* Transfer 9.99 *******' as '';
-CALL `transfer`(@accountNumber1, 1, @accountNumber1, 2, 9.99, "A transfer", @TransferSuccess);
+CALL `transfer`(@accountNumber1, 1, @accountNumber1, 2, 9.99, 'A transfer', @TransferSuccess);
 CALL `get_account_balance`(@accountNumber1);
 
 SELECT '******* Withdraw 90.01 *******' as '';
 SET @TransferSuccess = 0;
-CALL `withdraw`(@accountNumber1, 1, 90.01, "A withdraw", @TransferSuccess);
+CALL `withdraw`(@accountNumber1, 1, 90.01, 'A withdraw', @TransferSuccess);
 CALL `get_account_balance`(@accountNumber1);
 
 SELECT '******* Atempt (should fail) to withdraw a million dollars *******' as '';
 SET @TransferSuccess = 0;
-CALL `withdraw`(@accountNumber1, 2, 1000000, "Im a rich", @TransferSuccess);
+CALL `withdraw`(@accountNumber1, 2, 1000000, 'Im a rich', @TransferSuccess);
 CALL `get_account_balance`(@accountNumber1);
 
 SELECT '******* Transfer money into another person account *******' as '';
-CALL `transfer`(@accountNumber1, 1, @accountNumber2, 2, 1.00, "Take a dollar", @TransferSuccess);
+CALL `transfer`(@accountNumber1, 1, @accountNumber2, 2, 1.00, 'Take a dollar', @TransferSuccess);
 CALL `get_account_balance`(@accountNumber1);
 CALL `get_account_balance`(@accountNumber2);
 
-CALL `transfer`(@accountNumber1, 1, @accountNumber1, 1, 1.00, "Transfer to myself", @TransferSuccess);
+CALL `transfer`(@accountNumber1, 1, @accountNumber1, 1, 1.00, 'Transfer to myself', @TransferSuccess);
 
 SELECT '******* Show transaction history *******' as '';
 CALL `get_account_transaction_history`(@accountNumber1);
