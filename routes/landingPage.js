3fa4735731d457d869d8e2726d30bfcebc13ecea
Korbin Dansie <dansiekm@gmail.com>
2023-02-18 17:43:21 -0700

    landing page done

--- routes/landingPage.js
+++ routes/landingPage.js
@@ -1,14 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var dbCon = require("../lib/database");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landingPage', { });
+  console.log('Landing Page: GET');
+  let sql = "CALL get_account_balance(?);";
+  dbCon.query(sql, [req.session.accountNumber], function (err, rows) {
+    if (err) {
+      throw err;
+    }
+    console.log('Balance of the accounts is: ');
+    console.log(rows[0]);
+
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    const checkingAccountBalance = formatter.format(rows[0][0].balance);
+    const savingAccountBalance = formatter.format(rows[0][1].balance);
+
+    res.render('landingPage', { checkingAccount: checkingAccountBalance, savingAccount: savingAccountBalance});
+  });
+
 });
 
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
-  res.render('landingPage', { });
+  console.log('Landing Page: POST');
+
+  let sql = "CALL get_account_balance(?);";
+  dbCon.query(sql, [req.session.accountNumber], function (err, rows) {
+    if (err) {
+      throw err;
+    }
+    console.log('Balance of the accounts is: ');
+    console.log(rows[0]);
+
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    const checkingAccountBalance = formatter.format(rows[0][0].balance);
+    const savingAccountBalance = formatter.format(rows[0][1].balance);
+
+    res.render('landingPage', { checkingAccount: checkingAccountBalance, savingAccount: savingAccountBalance});
+  });
 }); 
 module.exports = router;
