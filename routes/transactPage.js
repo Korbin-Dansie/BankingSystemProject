9ef614a04ad9ddc650053fc752f3a0d5c5a8943b
Korbin Dansie <dansiekm@gmail.com>
2023-02-22 14:34:37 -0700

    deposit/withdraw is functional

--- routes/transactPage.js
+++ routes/transactPage.js
@@ -15,7 +15,39 @@ router.post("/", function (req, res, next) {
   console.log('transactPage.js POST');
   if (!req.session.loggedIn || req.session.loggedIn == false) {
     res.redirect("/");
-  } else {
+  } 
+  // We know if its a withdraw of deposit so do that action
+  else if(req.body.amount && req.body.accountType && req.body.administration){
+    console.log('transactPage.js deposit/withdraw');
+
+    const accountNumber = req.body.accountNumber;
+    const accountType = req.body.accountType;
+    const amount = req.body.amount;
+
+    // is it a deposit
+    if(req.body.administration == 'deposit'){
+      const sql = "CALL deposit(?,?,?, NULL, @result)";
+      dbCon.query(sql, [accountNumber, accountType, amount], function(err, rows){
+        if(err){
+          throw err;
+        }
+        res.redirect('/');
+      });
+    }
+    // is it a withdraw
+    else{
+      const sql = "CALL withdraw(?,?,?, NULL, @result)";
+      dbCon.query(sql, [accountNumber, accountType, amount], function(err, rows){
+        if(err){
+          throw err;
+        }
+        res.redirect('/');
+      });
+    }
+  }
+  // Have the account number - so disaplay if they want to do a deposit/withdraw
+  else {
+    console.log('transactPage.js get info to deposit/withdraw');
     const accountNumber = req.body.accountNumber;
 
     const sql = "CALL get_account_balance(?)";
@@ -32,6 +64,8 @@ router.post("/", function (req, res, next) {
 
       const checkingAccountBalance = formatter.format(rows[0][0].balance);
       const savingAccountBalance = formatter.format(rows[0][1].balance);
+      const checkingAccountNumber =  rows[0][0].balance;
+      const savingAccountNumber = rows[0][1].balance;
 
       // Get user fist and last name
       const sql = "SELECT u.user_first_name as firstName, u.user_last_name as lastName \n" +
@@ -49,8 +83,11 @@ router.post("/", function (req, res, next) {
         res.render("transactPageResults", {
           checkingAccount: checkingAccountBalance,
           savingAccount: savingAccountBalance,
+          checkingAccountNumber: checkingAccountNumber,
+          savingAccountNumber: savingAccountNumber,
           firstName: firstName,
           lastName: lastName,
+          accountNumber: accountNumber,
         });
       });
 
