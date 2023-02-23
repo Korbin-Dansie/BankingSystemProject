7d258a762786b732471eb18f2179f677d8069d05
Korbin Dansie <dansiekm@gmail.com>
2023-02-23 15:46:41 -0700

    added error handleing if account number does not exist

--- routes/transactPage.js
+++ routes/transactPage.js
@@ -65,45 +65,59 @@ router.post("/", function (req, res, next) {
     console.log('transactPage.js get info to deposit/withdraw');
     const accountNumber = req.body.accountNumber;
 
-    const sql = "CALL get_account_balance(?)";
-    dbCon.query(sql, [accountNumber], function (err, rows) {
-      if (err) {
+    //Check to make sure account exists
+    const sql = "CALL check_accountNumber(?);";
+    dbCon.query(sql, [accountNumber], function(err,rows){
+      if(err){
         throw err;
       }
-
-      // Get the customers accounts
-      const formatter = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      });
-
-      const checkingAccountBalance = formatter.format(rows[0][0].balance);
-      const savingAccountBalance = formatter.format(rows[0][1].balance);
-      const checkingAccountNumber =  rows[0][0].balance;
-      const savingAccountNumber = rows[0][1].balance;
-
-      // Get user fist and last name
-      const sql = "CALL getAccountName(?);";
-      dbCon.query(sql, [accountNumber], function(err, rows){
-        if(err){
-          throw err;
-        }
-        console.log(rows[0][0]);
-
-        const firstName = rows[0][0].firstName;
-        const lastName = rows[0][0].lastName;
-
-        res.render("transactPageResults", {
-          checkingAccount: checkingAccountBalance,
-          savingAccount: savingAccountBalance,
-          checkingAccountNumber: checkingAccountNumber,
-          savingAccountNumber: savingAccountNumber,
-          firstName: firstName,
-          lastName: lastName,
-          accountNumber: accountNumber,
+  
+      console.log(rows[0]);
+      // if account does not exist
+      if(rows[0][0].result == 0){
+        res.render('transactPage', { message: 'Account number: ' + accountNumber + " does not exist."});
+      }else{
+        const sql = "CALL get_account_balance(?)";
+        dbCon.query(sql, [accountNumber], function (err, rows) {
+          if (err) {
+            throw err;
+          }
+    
+          // Get the customers accounts
+          const formatter = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          });
+    
+          const checkingAccountBalance = formatter.format(rows[0][0].balance);
+          const savingAccountBalance = formatter.format(rows[0][1].balance);
+          const checkingAccountNumber =  rows[0][0].balance;
+          const savingAccountNumber = rows[0][1].balance;
+    
+          // Get user fist and last name
+          const sql = "CALL getAccountName(?);";
+          dbCon.query(sql, [accountNumber], function(err, rows){
+            if(err){
+              throw err;
+            }
+            console.log(rows[0][0]);
+    
+            const firstName = rows[0][0].firstName;
+            const lastName = rows[0][0].lastName;
+    
+            res.render("transactPageResults", {
+              checkingAccount: checkingAccountBalance,
+              savingAccount: savingAccountBalance,
+              checkingAccountNumber: checkingAccountNumber,
+              savingAccountNumber: savingAccountNumber,
+              firstName: firstName,
+              lastName: lastName,
+              accountNumber: accountNumber,
+            });
+          });
+    
         });
-      });
-
+      }
     });
   }
 });
